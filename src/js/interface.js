6b5c3b8d22d69c0b623e3b06edef51bb6570e6b2
Jan T. Sott <jan@idleberg.com>
2015-05-28 09:44:48 +0200

    update file

--- src/js/interface.js
+++ src/js/interface.js
@@ -151,13 +151,4 @@ $(function() {
     });
   });
 
-  $('#accordion').on('mouseenter.collapse', '[data-toggle=collapse]', function(e) {
-        var $this = $(this),
-            href, target = $this.attr('data-target') || e.preventDefault() || (href = $this.attr('href')) && href.replace(/.*(?=#[^\s]+$)/, '') //strip for ie7
-            ,
-            option = $(target).hasClass('in') ? 'hide' : "show"
-            $('.panel-collapse').not(target).collapse("hide")
-            $(target).collapse(option);
-    })
-
 });
